a268075224e9fa7c6f0cf8b120fcec7cabb8d6dd
frostyalchemite <Frosty.Dev@outlook.com>
2025-04-09 22:43:38 +0100

    Update FeliX.XYZ.lua

--- FeliX.XYZ.lua
+++ FeliX.XYZ.lua
@@ -1,45 +1,45 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
- 
+
 local Window = Rayfield:CreateWindow({
    Name = "🐱Felix.XYZ🐱",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "🐱Felix.XYZ🐱",
+   LoadingTitle = "Felix.XYZ",
    LoadingSubtitle = "by Big_Frosty",
    Theme = "Serenity", -- Check https://docs.sirius.menu/rayfield/configuration/themes
- 
+
    DisableRayfieldPrompts = false,
    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
- 
+
    ConfigurationSaving = {
       Enabled = true,
       FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = 🐱Felix.XYZ🐱GUI"
+      FileName = "FelixhubGUI"
    },
- 
+
    Discord = {
       Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
- 
+
    KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
-      Title = "🐱Felix.XYZ🐱 | key",
+      Title = "Felix.xyz | key",
       Subtitle = "Key System",
-      Note = "Key is a femboys fav word", -- Use this to tell the user how to get a key
-      FileName = "🐱Felix.XYZ🐱key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      Note = "Key must be obtained from script maker", -- Use this to tell the user how to get a key
+      FileName = "FelixKEY", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true , -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = {"UWU"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
- 
+
 local MainTab = Window:CreateTab("🏠 Home", nil) -- Title, Image
 local MainSection = MainTab:CreateSection("Main")
- 
+
 Rayfield:Notify({
-   Title = "You executed 🐱Felix.XYZ🐱",
-   Content = "🐱Felix.XYZ🐱 is now present",
+   Title = "You executed Felix.xyz",
+   Content = "Felix.xyz is now present",
    Duration = 5,
    Image = 13047715178,
    Actions = { -- Notification Buttons
@@ -51,20 +51,20 @@ Rayfield:Notify({
    },
 },
 })
- 
+
 local Button = MainTab:CreateButton({
    Name = "Infinite Jump Toggle",
    Callback = function()
        --Toggles the infinite jump between on or off on every script run
 _G.infinjump = not _G.infinjump
- 
+
 if _G.infinJumpStarted == nil then
 	--Ensures this only runs once to save resources
 	_G.infinJumpStarted = true
- 
+	
 	--Notifies readiness
 	game.StarterGui:SetCore("SendNotification", {Title="Youtube Hub"; Text="Infinite Jump Activated!"; Duration=5;})
- 
+
 	--The actual infinite jump
 	local plr = game:GetService('Players').LocalPlayer
 	local m = plr:GetMouse()
@@ -92,7 +92,7 @@ local Slider = MainTab:CreateSlider({
         game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
    end,
 })
- 
+
 local Slider = MainTab:CreateSlider({
    Name = "JumpPower Slider",
    Range = {1, 350},
